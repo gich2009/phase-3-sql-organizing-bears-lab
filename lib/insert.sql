0bb5ccbc903d5deacce19858317e175d219d7550
gich2009 <waneneamos@gmail.com>
2023-06-12 06:42:32 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Ms. Egghead', 10, 'M', 'Brown', 'Calm', 1),
+       ('Mr. Lay', 5, 'M', 'Black', 'Aggressive', 1),
+       ('Ms. Duckling', 7, 'F', 'Brown', 'Calm', 1),
+       ('Mr. Brown', 6, 'M', 'Brown', 'Serious', 1),
+       ('Ms. White', 3, 'F', 'White', 'Kind', 1),
+       ('Mr. Grass', 4, 'M', 'Green', 'Naughty', 0),
+       ('Mr. Snow', 9, 'F', 'White', 'Kind', 1),
+       (NULL, 7, 'M', 'Black', 'Unknown', 1);
